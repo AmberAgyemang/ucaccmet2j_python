d50919af7bc19f5c38a064a32f0bd6b4879055f6
Amber <ayaagyemang@gmail.com>
2020-01-14 14:05:11 +0100

    initiated file and described goal of assignment

---
+++ Station_file.py
@@ -0,0 +1,4 @@
+# Look into the csv file for the Seatle station
+# Use this code to select all the measurement that belong to it from the JSON file
+# Sum all the measurements of the location of Seatle for each month: create a list with the toal monthly precipation
+# Safe as a JSON file
